d1ecf6a9d095ce868f4a1ae4b2bb0ca1307e63da
Saurabh Kumar Singh <saurabhtk1993@gmail.com>
2024-12-29 10:18:12 +0000

    Done

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33aaa";
+ //console.log(typeof score)
+ let convertedNumber = Number(score)
+//  console.log(typeof convertedNumber)
+//  console.log(convertedNumber)
+ 
+let loggedIn = ""
+let booleanloggedIn = Boolean(loggedIn)
+console.log(typeof booleanloggedIn)
+console.log(booleanloggedIn)
---
+++ 01_basic/variables.js
@@ -0,0 +1,10 @@
+let person = "saurabhSingh";
+//person = "singh is king";
+const mob = 7053657687;
+address = "Gangani"
+
+
+console.log(person);
+console.log(mob);
+console.log(address)
+console.table([typeof person, typeof mob, typeof address])
