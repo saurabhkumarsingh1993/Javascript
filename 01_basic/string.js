1b20cb11f8536396d23ebb92e4301e5fa0e7c4da
Saurabh Kumar Singh <saurabhtk1993@gmail.com>
2024-12-31 11:14:06 +0000

    done

---
+++ 01_basic/string.js
@@ -0,0 +1,12 @@
+let personName = "SaurabhS"
+const personAddress = "Gangani"
+let repoCount = 50
+//  console.log(personName.length)
+//  console.log(personName.charAt(1))
+//  console.log(personAddress.concat('firozabad'))
+//quotes with in the double quotes 
+ let answere = "Hi my name is saurabh kumar and i'm from agra"
+ console.log(answere)
+ //Template Strings
+ let txt = `He's often called "Johnny"`
+ console.log(txt)
